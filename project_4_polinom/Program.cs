6427f33ac8b13ff147f86ff2a66ec2f025349a3d
Dimo <Dimo@RIN>
2024-06-05 06:39:35 +0300

    Add project files.

---
+++ project_4_polinom/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Въведете коефициентите на полином, разделени с интервали:");
+        string input = Console.ReadLine();
+
+        // Разделяме входа и го преобразуваме в масив от числа
+        double[] coefficients = input.Split(' ').Select(double.Parse).ToArray();
+
+        // Извеждаме коефициентите
+        Console.WriteLine("Коефициентите на полинома са:");
+        foreach (var coefficient in coefficients)
+        {
+            Console.WriteLine(coefficient);
+        }
+    }
+}
